ce185e6636b49c88a2698a7ec06b4e2ffc36234c
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-24 21:38:42 +0300

    Added basic models generation using a text template which inherits data from the edmx

--- Selly.BusinessLogic/Core/ClientCore.cs
+++ Selly.BusinessLogic/Core/ClientCore.cs
@@ -1,4 +1,7 @@
-﻿namespace Selly.BusinessLogic.Core
+﻿using Selly.BusinessLogic.Core.Base;
+using Selly.DataLayer.Repositories;
+
+namespace Selly.BusinessLogic.Core
 {
     public static class ClientCore
     {
