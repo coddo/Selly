a064c313d8ffc04247c51f597ce3d1fb1a50f4eb
claudiu codoban <claudiu.codoban@deventure.co>
2016-05-31 22:18:09 +0300

    Added order pages in UI.

---
+++ Selly.Website/App/Controllers/OrderController.js
@@ -0,0 +1,31 @@
+﻿angular
+    .module('selly')
+    .controller('OrderController', function ($scope, API, HelperService, $routeParams) {
+
+
+        $scope.client = {};
+
+        function init() {
+            loadClient($routeParams.clientId);
+        }
+        init();
+
+
+        function loadClient(clientId) {
+            HelperService.StartLoading('loadClients');
+            API.getClient({ clientId: clientId }, function (success) {
+                $scope.client = success.data;
+                HelperService.StopLoading('loadClients');
+
+                if (!success.isSuccess)
+                    HelperService.ShowMessage('alert-danger', 'An error has occured! Try again!');
+            }, function (error) {
+                HelperService.StopLoading('loadClients');
+                HelperService.ShowMessage('alert-danger', 'An error has occured! Try again!');
+            });
+        };
+
+
+        
+
+    });
