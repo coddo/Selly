cb5bcf9927c0d4f4adeadf918d2bd30db31394b8
claudiu codoban <claudiu.codoban@deventure.co>
2016-06-01 17:28:38 +0300

    Improved tt logic and reverted the ClientRepository to a valid state

--- Selly.DataLayer/Repositories/ClientRepository.cs
+++ Selly.DataLayer/Repositories/ClientRepository.cs
@@ -1,25 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq.Expressions;
-using Selly.DataLayer.Repositories.Base;
+﻿using Selly.DataLayer.Repositories.Base;
 
 namespace Selly.DataLayer.Repositories
 {
-    public class ClientRepository : BaseRepository<Client>
+    public class ClientRepository : BaseSinglePkRepository<Client>
     {
-        protected override Expression<Func<Client, bool>> GetFindByIdQuery(IList<Guid> primaryKeys)
-        {
-            if (primaryKeys == null || primaryKeys.Count != 2)
-            {
-                return null;
-            }
-
-            return entity => entity.Id == primaryKeys[0] && entity.CurrencyId == primaryKeys[1];
-        }
-
-        protected override bool ValidateEntity(Client entity)
-        {
-            return entity != null && entity.Id != Guid.Empty && entity.CurrencyId != Guid.Empty;
-        }
     }
 }
