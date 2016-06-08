7df7fb49e9b580ffd21246ffd7b68347f9c62a68
claudiu codoban <claudiu.codoban@deventure.co>
2016-06-08 23:49:34 +0300

    Added unit tests for client cruds

---
+++ Selly.BusinessLogic.Tests/Core/ClientCoreTest.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Selly.BusinessLogic.Core;
+using Selly.Models;
+using Selly.Models.Common.Response;
+
+namespace Selly.BusinessLogic.Tests.Core
+{
+    [TestClass]
+    public class ClientCoreTest
+    {
+        private static Client mClient;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            CreateClient();
+        }
+
+        [ClassCleanup]
+        public static void Cleanup()
+        {
+            DeleteClient();
+        }
+
+        [TestMethod]
+        public void UpdateClientTest()
+        {
+            var newClient = EntityHelper.GenerateClient();
+            newClient.Id = mClient.Id;
+
+            var response = ClientCore.UpdateAsync(newClient, true).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            AssertObjects(response, newClient);
+        }
+
+        [TestMethod]
+        public void GetClientTest()
+        {
+            var response = ClientCore.GetAsync(mClient.Id).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            AssertObjects(response, mClient);
+        }
+
+        [TestMethod]
+        public void GetAllClientsTest()
+        {
+            var response = ClientCore.GetAllAsync().ConfigureAwait(false).GetAwaiter().GetResult();
+
+            response.Should().NotBeNull();
+            response.Data.Should().NotBeNull();
+            response.Data.Count.Should().NotBe(0);
+        }
+
+        private static void CreateClient()
+        {
+            mClient = EntityHelper.GenerateClient();
+
+            var response = ClientCore.CreateAsync(mClient, true).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            AssertObjects(response, mClient);
+        }
+
+        private static void DeleteClient()
+        {
+            var response = ClientCore.DeleteAsync(mClient).ConfigureAwait(false).GetAwaiter().GetResult();
+
+            response.Should().NotBeNull();
+            response.Success.Should().BeTrue();
+        }
+
+        private static void AssertObjects(Response<Client> response, Client client)
+        {
+            response.Should().NotBeNull();
+            response.Success.Should().BeTrue();
+            response.Data.Should().NotBeNull();
+            response.Data.Should().NotBeNull();
+            response.Data.CurrencyId.Should().Be(client.CurrencyId);
+            response.Data.Email.Should().Be(client.Email);
+            response.Data.FirstName.Should().Be(client.FirstName);
+            response.Data.LastName.Should().Be(client.LastName);
+            response.Data.Id.Should().Be(client.Id);
+        }
+    }
+}
---
+++ Selly.BusinessLogic.Tests/EntityHelper.SingleEntities.cs
@@ -0,0 +1,19 @@
+﻿using Ploeh.AutoFixture;
+using Selly.Models;
+
+namespace Selly.BusinessLogic.Tests
+{
+    public static partial class EntityHelper
+    {
+        private static readonly Fixture mFixture = new Fixture { Behaviors = { new OmitOnRecursionBehavior() } };
+
+        public static Client GenerateClient()
+        {
+            return mFixture.Build<Client>()
+                .Without(client => client.Currency)
+                .Without(client => client.Orders)
+                .Without(client => client.Payrolls)
+                .Create();
+        }
+    }
+}
