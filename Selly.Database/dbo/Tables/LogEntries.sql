9a8c771410b93f399d9331abc288db5269ba47bb
claudiu codoban <claudiu.codoban@deventure.co>
2016-06-08 23:13:36 +0300

    Added a logging service and made the necessary configurations

---
+++ Selly.Database/dbo/Tables/LogEntries.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[LogEntries] (
+    [ID]        INT            IDENTITY (1, 1) NOT NULL,
+    [TimeStamp] DATETIME2 (7)  NULL,
+    [Level]     NVARCHAR (15)  NULL,
+    [Logger]    NVARCHAR (128) NULL,
+    [Exception] NVARCHAR (MAX) NULL,
+    [Message]   NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
